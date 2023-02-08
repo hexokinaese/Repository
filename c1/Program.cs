e79e5ede83177ffd5b5104996d87937ceb30e194
reminiscence <121186441+hexokinaese@users.noreply.github.com>
2023-02-08 15:46:02 +0200

    Add files via upload

---
+++ c1/Program.cs
@@ -0,0 +1,17 @@
+﻿byte age = 12;
+double bankacc = 219.324335;
+char letter = 'c';
+string message = "Hello, World!";
+bool statement = false;
+float humantemp = 36.7f;
+ulong population = 43790000;
+float coretemp = 9.38f;
+
+Console.WriteLine(age);
+Console.WriteLine(bankacc);
+Console.WriteLine(letter);
+Console.WriteLine(message);
+Console.WriteLine(statement);
+Console.WriteLine(humantemp);
+Console.WriteLine(population);
+Console.WriteLine(coretemp);
