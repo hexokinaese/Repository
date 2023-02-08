6e714bca1a9cec0b4287216addeae2a992729355
reminiscence <121186441+hexokinaese@users.noreply.github.com>
2023-02-08 19:43:52 +0200

    Delete 1.cs

--- 1.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.WriteLine("Enter login: ");
-string login = Console.ReadLine();
-
-string pass = (login == "user") ? "Enter password: " : "Wrong user!";
-Console.WriteLine(pass);
-string password = Console.ReadLine();
-string pass1 = (password == "secretpass") ? "Hello user" : "Wrong password";
-Console.WriteLine(pass1);
