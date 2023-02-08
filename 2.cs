ac463c74f4ee18782fe8906983871c1104e37a09
reminiscence <121186441+hexokinaese@users.noreply.github.com>
2023-02-08 19:45:03 +0200

    Delete 2.cs

--- 2.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.WriteLine("Enter login: ");
-string login = Console.ReadLine();
-
-string pass = (login == "admin") ? "Enter password: " : "Wrong user!";
-Console.WriteLine(pass);
-string password = Console.ReadLine();
-string pass1 = (password == "secretpass") ? "Hello Administrator" : "Wrong password";
-Console.WriteLine(pass1);
