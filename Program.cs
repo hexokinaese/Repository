d04675829c97d81afeaadc920b1631c4838cb6b4
reminiscence <121186441+hexokinaese@users.noreply.github.com>
2023-02-16 16:17:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿int i, j, N = 11;
+int center = N / 2;
+
+for (i = 0; i < N; i++)
+{
+    for (j = 0; j < N; j++)
+    {
+        if (i <= center)
+        {
+            if (j >= center - i && j <= center + i)
+                Console.Write("*");
+            else
+                Console.Write(" ");
+        }
+        else
+        {
+            if (j >= center + i - N + 1 && j <= center - i + N - 1)
+                Console.Write("*");
+            else
+                Console.Write(" ");
+        }
+    }
+    Console.WriteLine();
+}
+return 0;
